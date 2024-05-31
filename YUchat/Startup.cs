49dc7e95a56e873ccea51367d7c911298a02cd35
Elena Pejovic <elenapejovic.ep@gmail.com>
2024-05-31 02:00:58 +0200

    Landing page

--- YUchat/Startup.cs
+++ YUchat/Startup.cs
@@ -1,14 +1,37 @@
 ﻿using Microsoft.Owin;
 using Owin;
+using System.IO;
+using Microsoft.Owin.Cors;
+using Microsoft.Owin.FileSystems;
+using Microsoft.Owin.StaticFiles;
+using System;
 
 [assembly: OwinStartup(typeof(YUchat.Startup))]
+
 namespace YUchat
 {
-    public partial class Startup
+    public class Startup
     {
         public void Configuration(IAppBuilder app)
         {
-            //ConfigureAuth(app);
+            // OWIN specific configuration
+            app.UseCors(CorsOptions.AllowAll);
+
+            // Static file configuration
+            var frontendPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "frontend");
+            var physicalFileSystem = new PhysicalFileSystem(frontendPath);
+            var options = new Microsoft.Owin.StaticFiles.FileServerOptions
+            {
+                EnableDefaultFiles = true,
+                FileSystem = physicalFileSystem
+            };
+            options.StaticFileOptions.FileSystem = physicalFileSystem;
+            options.StaticFileOptions.ServeUnknownFileTypes = true;
+            options.DefaultFilesOptions.DefaultFileNames = new[] { "index.html" };
+
+            app.UseFileServer(options);
+
+            // SignalR configuration
             app.MapSignalR();
         }
     }
---
+++ YUchat/frontend/js/JavaScript.js
@@ -0,0 +1,31 @@
+﻿$(document).ready(function () {
+    // Primer AJAX poziva ka backend API-ju
+    $.ajax({
+        url: 'http://localhost:5000/api/data', // URL vašeg backend API-ja
+        method: 'GET',
+        success: function (data) {
+            console.log(data);
+            // Ovde možete obraditi podatke i prikazati ih na stranici
+            $('#content').html(JSON.stringify(data));
+        },
+        error: function (error) {
+            console.error('Error fetching data:', error);
+        }
+    });
+
+    // SignalR konfiguracija
+    const connection = new signalR.HubConnectionBuilder()
+        .withUrl("http://localhost:5000/chatHub") // URL vašeg SignalR huba
+        .build();
+
+    connection.on("ReceiveMessage", function (user, message) {
+        const msg = $('<div></div>').text(user + ": " + message);
+        $('#content').append(msg);
+    });
+
+    connection.start().then(function () {
+        console.log('SignalR connected');
+    }).catch(function (err) {
+        return console.error(err.toString());
+    });
+});
